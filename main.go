8ac369007819197891f02ee845e56682a88838ce
Charles Neill <1749665+cneill@users.noreply.github.com>
2024-01-17 18:50:53 -0600

    dev commit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
@@ -11,4 +12,6 @@ func main() {
 	pieces := strings.Fields(command)
 	cmd := exec.Command(pieces[0], pieces[1:]...)
 	cmd.Run()
+
+	fmt.Printf("I just added one line but the vuln is still the same")
 }
