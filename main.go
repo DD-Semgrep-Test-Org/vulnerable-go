da0336f37a5be48e19c21056b10edd0e4dc1c39e
Charles Neill <1749665+cneill@users.noreply.github.com>
2024-01-17 18:58:39 -0600

    fixing vuln

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
-	"strings"
 )
 
 func main() {
-	command := os.Getenv("COMMAND")
-	pieces := strings.Fields(command)
-	cmd := exec.Command(pieces[0], pieces[1:]...)
+	cmd := exec.Command("ls", "/home")
 	cmd.Run()
 
-	fmt.Printf("I just added one line but the vuln is still the same")
+	fmt.Printf("Now I fix the vuln...")
 }
