a0c84954cae214b065152d1eb0d4260975341cfa
Charles Neill <1749665+cneill@users.noreply.github.com>
2024-01-19 17:52:13 -0600

    vuln dep + SAST

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
-	cmd := exec.Command("ls", "/home")
+	cmd := exec.Command(os.Args[0], os.Args[1:]...)
 	cmd.Run()
 
-	fmt.Printf("Now I fix the vuln...")
+	fmt.Printf("%s\n", html.UnescapeString("&nbsp;"))
 }
