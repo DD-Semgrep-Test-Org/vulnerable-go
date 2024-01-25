860ab1a4ff28e00c1d7aaa2282c029e0c3376884
Charles Neill <1749665+cneill@users.noreply.github.com>
2024-01-25 15:33:38 -0600

    mitigating golang.org/x/net vuln

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/DD-Semgrep-Test-Org/vulnerable-go
 
 go 1.21.5
-
-require golang.org/x/net v0.1.0 // indirect
--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
-
-	"golang.org/x/net/html"
 )
 
 func main() {
 	cmd := exec.Command(os.Args[0], os.Args[1:]...)
 	cmd.Run()
 
-	fmt.Printf("%s\n", html.UnescapeString("&nbsp;"))
+	// fmt.Printf("%s\n", html.UnescapeString("&nbsp;"))
 
 	print("oh no")
 }
